97d2e8e55dda8489e100eb405bcd6a3c4a8d8826
Emil Nilsson <emil.git.nilsson@gmail.com>
2021-03-18 12:24:08 +0100

    Add function updateCurrentNum

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
-console.log("HELLO WORLD");
+const btns = document.getElementById('calculator');
+
+const numObj = {
+    currentNum: null,
+    prevNum: null,
+    prevOperator: null
+}
+
+
+btns.addEventListener('mousedown', (evt) => {
+    const btnPressed = evt.target.textContent;
+    
+    if (btnPressed != '') {
+        console.log(btnPressed)
+        console.log(typeof btnPressed)
+    
+        switch (btnPressed) {
+
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                // Update currentNum with btnPressed
+                updateCurrentNum(btnPressed)
+                break;
+
+
+            case '=':
+                // Calculate savedNum and currentNum using currentOperator
+
+                // Overwrite savedNum with result
+
+                break; 
+
+            case 'del':
+                // Delete latest entered digit/comma from currentNum
+                break;
+
+            case ',':
+
+                break;
+
+            case '/':
+            case '*':
+            case '-':
+            case '+':
+
+                break;
+
+            
+
+            case '0':
+        }
+
+    }
+
+});
+
+function updateCurrentNum(digit) {
+    if (numObj.currentNum == null) {
+        numObj.currentNum = digit
+    }
+    else {
+        numObj.currentNum += digit;
+    }
+}
